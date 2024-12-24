695888a29f9c4e38ca1c9633ee6313323dc8a44a
Giancarlo Bonvenuto <giancarlo.bonvenuto@gmail.com>
2024-12-24 17:25:50 -0300

    Implementando numeros

---
+++ src/LexicalAnalyzer.zig
@@ -0,0 +1,68 @@
+const std = @import("std");
+
+/// Este struct possui diversas funções para análise lexical
+pub const Lex = struct {
+    pub const Keywords = enum {
+        @"if",
+        then,
+        @"else",
+    };
+    pub const Token = struct {
+        value: *void,
+        type: Types,
+        pub const Types = enum {
+            integer,
+            float,
+            char,
+            op,
+            keyword,
+        };
+    };
+    pub fn numbers(content: []const u8, initial_index: usize, allocator: std.mem.Allocator) !struct { token: Token, index: usize } {
+        var index = initial_index;
+        var tok_type = Token.Types.integer;
+
+        var value: usize = 0;
+        while (std.ascii.isDigit(content[index])) : (index = index + 1) {
+            value *= 10;
+            value += content[index] - '0';
+        }
+
+        // Se tiver um ponto, então é um float e devemos continuar lendo
+        if (content[index] == '.') {
+            var decimal_value_i: usize = 0;
+            const point_index = index;
+            index += 1;
+            if (!std.ascii.isDigit(content[index])) {
+                return error.FloatMalformed;
+            }
+            while (std.ascii.isDigit(content[index])) : (index += 1) {
+                decimal_value_i *= 10;
+                decimal_value_i += content[index] - '0';
+            }
+
+            const decimal_value: f32 = @floatFromInt(decimal_value_i);
+            const div: f32 = @floatFromInt(index-point_index);
+            const value_f: f32 = @floatFromInt(value);
+            const decimal_float: f32 = (decimal_value / std.math.pow(f32, 10, div-1)) + value_f;
+
+            tok_type = Token.Types.float;
+            const tok_value = try allocator.create(f32);
+            tok_value.* = decimal_float;
+            return .{ .token = Token{
+                .value = @ptrCast(tok_value),
+                .type = tok_type,
+            }, .index = index - 1 };    
+
+        }
+        // Caso contrário é um inteiro e já lemos tudo
+        else {
+            const tok_value = try allocator.create(usize);
+            tok_value.* = value;
+            return .{ .token = Token{
+                .value = @ptrCast(tok_value),
+                .type = tok_type,
+            }, .index = index - 1 };
+        }
+    }
+};
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const hashmap = @import("./hashmap.zig");
+const Lex = @import("./LexicalAnalyzer.zig").Lex;
 
 const stdin = std.io.getStdIn().reader();
 const stdout = std.io.getStdOut().writer();
@@ -29,79 +30,81 @@ pub fn main() !void {
 
     if (file_name) |f| {
         variablesHashMap = try hashmap.VariablesHashMap.init(allocator);
-        tokenizer(f) catch |err| switch (err) {
+        // Abrindo o arquivo
+        var file = std.fs.cwd().openFile(f, .{ .mode = .read_only }) catch |err| switch (err) {
+            error.FileNotFound => {
+                std.debug.print("File Not found!", .{});
+                return;
+            },
+            else => return err,
+        };
+        defer file.close();
+
+        const content = try file.readToEndAlloc(allocator, std.math.maxInt(usize));
+        defer allocator.free(content);
+
+        // Deixando o conteúdo imutável (boas práticas)
+        const file_content: []const u8 = content;
+
+        tokenizer(file_content, allocator) catch |err| switch (err) {
             else => return err,
         };
     } else {
-        try interactive();
+        try interactive(allocator);
     }
 }
 
-fn interactive() !void {
+fn interactive(allocator: std.mem.Allocator) !void {
     try stdout.print("Marte 0.0.1 Copyright (C) 2024 Giancarlo Bonvenuto\n", .{});
 
     while (true) {
         try stdout.print("> ", .{});
         var line: [1000]u8 = undefined;
         _ = try stdin.readUntilDelimiter(&line, '\n');
-        tokenizer(file_name: []const u8)
+        try tokenizer(&line, allocator);
     }
 }
 
 // Tokens reconhecidos
-const Tokens = enum {
-    @"if",
-    then,
-    @"else",
-};
 
-pub fn tokenizer(file_name: []const u8) !void {
-    // Abrindo o arquivo
-    var file = std.fs.cwd().openFile(file_name, .{ .mode = .read_only }) catch |err| switch (err) {
-        error.FileNotFound => {
-            std.debug.print("File Not found!", .{});
-            return;
-        },
-        else => return err,
-    };
-    defer file.close();
-
-    const allocator = std.heap.page_allocator;
-    const content = try file.readToEndAlloc(allocator, std.math.maxInt(usize));
-    defer allocator.free(content);
-
-    // Deixando o conteúdo imutável (boas práticas)
-    const file_content: []const u8 = content;
+pub fn tokenizer(content: []const u8, allocator: std.mem.Allocator) !void {
 
     // Iterando pelas palavras
 
     // var varName: []const u8 = undefined;
-    var evVar: bool = false;
+    // var evVar: bool = false;
 
-    for (file_content, 0..) |char, i| {
-
-        // Iterando pelo Enum de tokens
-        var word: []u8 = @constCast(&[_]u8{0} ** 100);
-        word[0] = char;
-        inline for (1..100) |j| {
-            if (std.ascii.isWhitespace(file_content[i + j])) {
-                word[j] = 0;
-                break;
-            }
-
-            word[j] = file_content[i + j];
+    var i: usize = 0;
+    while (i < content.len) : (i += 1) {
+        const char = content[i];
+        if (char >= '0' and char <= '9') {
+            const ret = try Lex.numbers(content, i, allocator);
+            const token = ret.token;
+            i = ret.index;
+            const value: *f32 = @alignCast(@ptrCast(token.value));
+            std.debug.print("token: {any}, value: {any}\n", .{ token, value.* });
         }
-        inline for (@typeInfo(Tokens).Enum.fields) |token| {
-            if (std.mem.eql(u8, token.name, word)) {
-                std.debug.print("{s} == {s}\n", .{ word, token.name });
-                break;
-            }
-        }
-        // Se não é um token especial, então é uma variável
-        // Pegamos o nome dela
-        evVar = true;
 
-        try handleVars(word);
+        // switch (char) {
+        // }
+
+        // // Iterando pelo Enum de tokens
+        // var word: []u8 = @constCast(&[_]u8{0} ** 100);
+        // word[0] = char;
+        // inline for (1..100) |j| {
+        //     if (std.ascii.isWhitespace(content[i + j])) {
+        //         word[j] = 0;
+        //         break;
+        //     }
+        //
+        //     word[j] = content[i + j];
+        // }
+        // inline for (@typeInfo(Tokens).Enum.fields) |token| {
+        //     if (std.mem.eql(u8, token.name, word)) {
+        //         std.debug.print("{s} == {s}\n", .{ word, token.name });
+        //         break;
+        //     }
+        // }
     }
 }
 
